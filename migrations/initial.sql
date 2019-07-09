29c30224bcdf0a92014b77517d5d2cd63fbcefa2
Michiel Sikma <michiel@sikma.org>
2019-07-10 01:16:03 +0200

    Add initial version of database schema

---
+++ migrations/initial.sql
@@ -0,0 +1,433 @@
+-- MariaDB dump 10.17  Distrib 10.4.6-MariaDB, for osx10.14 (x86_64)
+--
+-- Host: localhost    Database: archivetube
+-- ------------------------------------------------------
+-- Server version	10.3.15-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activity_logs`
+--
+
+DROP TABLE IF EXISTS `activity_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activity_logs` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `namespace` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `time_utc` datetime DEFAULT NULL,
+  `entity` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `entity_id` bigint(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activity_logs`
+--
+
+LOCK TABLES `activity_logs` WRITE;
+/*!40000 ALTER TABLE `activity_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activity_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `channels`
+--
+
+DROP TABLE IF EXISTS `channels`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `channels` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) unsigned DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `description_short` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description_short_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `homepage` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `subscribers` int(10) unsigned NOT NULL DEFAULT 0,
+  `created_utc` datetime DEFAULT NULL,
+  `latest_activity_utc` datetime DEFAULT NULL,
+  `country_iso_code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ISO 3166-1 alpha-3 codes',
+  `country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `channel_views` int(11) NOT NULL DEFAULT 0,
+  `videos` int(11) NOT NULL DEFAULT 0,
+  `comments` int(11) unsigned NOT NULL DEFAULT 0,
+  `last_comment_utc` datetime DEFAULT NULL,
+  `banned_until_utc` datetime DEFAULT NULL,
+  `layout_json` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_main_channel` tinyint(1) NOT NULL DEFAULT 0,
+  `is_commentable` tinyint(1) NOT NULL DEFAULT 1,
+  `is_perm_banned` tinyint(1) NOT NULL DEFAULT 0,
+  `is_temp_banned` tinyint(1) NOT NULL DEFAULT 0,
+  `is_unlisted` tinyint(1) NOT NULL DEFAULT 0,
+  `is_private` tinyint(1) NOT NULL DEFAULT 0,
+  `is_retired` tinyint(1) NOT NULL DEFAULT 0,
+  `is_locked` tinyint(1) NOT NULL DEFAULT 0,
+  `is_closed` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `channels`
+--
+
+LOCK TABLES `channels` WRITE;
+/*!40000 ALTER TABLE `channels` DISABLE KEYS */;
+/*!40000 ALTER TABLE `channels` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) unsigned DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `language` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `creation_utc` datetime DEFAULT NULL,
+  `last_edit_utc` datetime DEFAULT NULL,
+  `is_spam_flagged` tinyint(1) NOT NULL DEFAULT 0,
+  `is_spam` tinyint(1) NOT NULL DEFAULT 0,
+  `is_edited` tinyint(1) NOT NULL DEFAULT 0,
+  `is_hidden` tinyint(1) NOT NULL DEFAULT 0,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `is_created_by_bot` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorite_video_groups`
+--
+
+DROP TABLE IF EXISTS `favorite_video_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorite_video_groups` (
+  `user_id` bigint(11) unsigned NOT NULL,
+  `video_group_id` bigint(11) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`video_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorite_video_groups`
+--
+
+LOCK TABLES `favorite_video_groups` WRITE;
+/*!40000 ALTER TABLE `favorite_video_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favorite_video_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorite_videos`
+--
+
+DROP TABLE IF EXISTS `favorite_videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorite_videos` (
+  `user_id` bigint(11) unsigned NOT NULL,
+  `video_id` bigint(11) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorite_videos`
+--
+
+LOCK TABLES `favorite_videos` WRITE;
+/*!40000 ALTER TABLE `favorite_videos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favorite_videos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media_files`
+--
+
+DROP TABLE IF EXISTS `media_files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media_files` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `video_id` bigint(11) unsigned DEFAULT NULL,
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `filesize` bigint(11) DEFAULT NULL,
+  `created_utc` datetime DEFAULT NULL,
+  `added_utc` datetime DEFAULT NULL,
+  `type` enum('video','audio') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `quality` enum('240p','360p','720p','1080p') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `original_filename` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `original_uri` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `video_codec` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `audio_codec` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `decoded_format` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `encoded_by` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `resolution_width` smallint(5) DEFAULT NULL,
+  `resolution_height` smallint(5) DEFAULT NULL,
+  `framerate` float DEFAULT NULL,
+  `channels` enum('mono','stereo') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sample_rate` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `artist` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `album` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `track_number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `disc_number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `genre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media_files`
+--
+
+LOCK TABLES `media_files` WRITE;
+/*!40000 ALTER TABLE `media_files` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media_files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `playlists`
+--
+
+DROP TABLE IF EXISTS `playlists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playlists` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) unsigned DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `identifier` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `creator` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `language` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `publication_utc` datetime DEFAULT NULL,
+  `visibility` enum('public','unlisted','private') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'public',
+  `is_created_by_bot` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playlists`
+--
+
+LOCK TABLES `playlists` WRITE;
+/*!40000 ALTER TABLE `playlists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `playlists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profiles`
+--
+
+DROP TABLE IF EXISTS `profiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profiles` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) unsigned DEFAULT NULL,
+  `layout_json` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `display_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `real_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bio` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bio_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `homepage` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country_iso_code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_activity_utc` datetime DEFAULT NULL,
+  `avatar_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar_width` smallint(5) DEFAULT NULL,
+  `avatar_height` smallint(5) DEFAULT NULL,
+  `is_unlisted` tinyint(1) NOT NULL DEFAULT 0,
+  `is_private` tinyint(1) NOT NULL DEFAULT 0,
+  `is_bot` tinyint(1) NOT NULL DEFAULT 0,
+  `is_individual` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profiles`
+--
+
+LOCK TABLES `profiles` WRITE;
+/*!40000 ALTER TABLE `profiles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subscribers`
+--
+
+DROP TABLE IF EXISTS `subscribers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subscribers` (
+  `user_id_parent` bigint(11) unsigned NOT NULL,
+  `user_id_child` bigint(11) unsigned NOT NULL,
+  PRIMARY KEY (`user_id_parent`,`user_id_child`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subscribers`
+--
+
+LOCK TABLES `subscribers` WRITE;
+/*!40000 ALTER TABLE `subscribers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subscribers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_backup` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `requires_pw_change` tinyint(1) NOT NULL DEFAULT 0,
+  `invalid_attempts` int(11) NOT NULL DEFAULT 0,
+  `birthdate_utc` date DEFAULT NULL,
+  `created_utc` datetime DEFAULT NULL,
+  `banned_until_utc` datetime DEFAULT NULL,
+  `ip_lock` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `2fa_key` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_temp_banned` tinyint(1) NOT NULL DEFAULT 0,
+  `is_perm_banned` tinyint(1) NOT NULL DEFAULT 0,
+  `is_ip_locked` tinyint(1) NOT NULL DEFAULT 0,
+  `is_2fa_locked` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `video_groups`
+--
+
+DROP TABLE IF EXISTS `video_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `video_groups` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) unsigned DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `identifier` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `creator` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `language` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `publication_utc` datetime DEFAULT NULL,
+  `visibility` enum('public','unlisted','private') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'public',
+  `is_created_by_bot` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `video_groups`
+--
+
+LOCK TABLES `video_groups` WRITE;
+/*!40000 ALTER TABLE `video_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `video_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `videos` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) unsigned DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description_format` enum('txt','md','html') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'txt',
+  `identifier` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `creator` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `language` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `upload_utc` datetime DEFAULT NULL,
+  `publication_utc` datetime DEFAULT NULL,
+  `comments` int(11) NOT NULL DEFAULT 0,
+  `last_comment_utc` datetime DEFAULT NULL,
+  `visibility` enum('public','unlisted','private') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'public',
+  `classification` enum('sfw','nsfw') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'sfw',
+  `ia_identifier` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ia_collection` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_hidden` tinyint(1) NOT NULL DEFAULT 0,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `is_uploaded_by_bot` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `videos`
+--
+
+LOCK TABLES `videos` WRITE;
+/*!40000 ALTER TABLE `videos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `videos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-10  1:15:03
